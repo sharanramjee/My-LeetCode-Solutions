63f354f53c830a5c4fc7d91a52467ddada971aeb
Sharan Ramjee <sharanramjee@gmail.com>
2018-10-02 23:45:21 -0400

    Jewels and Stones
    Jewels and Stones

---
+++ JewelsAndStones.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for char in S:
+            if char in J:
+                count += 1
+        return count
+        
